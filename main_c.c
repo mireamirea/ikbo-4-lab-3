e65f9d578a1a4d20daa25dfa01069ebbc165fa68
markorolek <marina_kuzmina_1998@mail.ru>
2017-06-07 17:34:10 +0400

    Add files via upload

---
+++ main_c.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define CHILD 1
+#define BROTHERS 2
+#define N 255
+
+struct node* crnode(int val);
+int add_tree(struct node *root, int val,int a,int b);
+struct node* find_tree(struct node *root, int val);
+void print_tree(struct node *root, int stage);
+int obhod(struct node *root, int *nnode, int *inner);
+struct node * roditeli(struct node *root, int val);
+int categorization(FILE *file, struct node *root);
+
+//описание структуры, хранящей узел дерева
+typedef struct node
+{
+	int key;
+	struct node *left, *right;
+} node;
+
+int i, rd, wt, nnode, inner;
+int main(int argc,char *argv[])
+{
+    FILE *rd_from, *wt_to;
+
+    char *p = (char*)malloc (sizeof(char)*N);
+    if (argc==1){
+        printf("application %s methods\n", argv[0]);
+        printf(":rd-rd_from\n:wt-wt_to\n");
+    }
+    for (i=1;i<argc;i++)
+    if (strstr(argv[i], ":rd")==argv[i]){
+        strcpy(p, 2+(strstr(argv[i],":rd")));
+        if (fopen(p,"r"))
+            rd_from = fopen(p, "r");
+        else{
+            printf("%s - error of reading\n", p);
+            continue;
+        }
+        rd = 1;
+    }
+        else
+        if (strstr(argv[i],":wt")==argv[i]){
+            strcpy(p, 2+(strstr(argv[i], ":wt")));
+            wt_to = fopen(p, "w");
+
+        wt = 1;
+        }
+    int val, b;
+    struct node* root;
+
+    if (rd){
+        fscanf(rd_from, "root %d", &val);
+        root = crnode(val);
+        while(fscanf(rd_from, "%d roditel %d", &val, &b)==2)
+            add_tree(root,val,CHILD,b);
+        fclose(rd_from);
+    }
+        else{
+            printf("root\n");
+            scanf("%d", &val);
+            root = crnode(val);
+        }
+    printf("enter instruction:(value of value like CHILD/BROTHERS\n)");
+
+    char a[30];
+    while (scanf("%d of %d like %s", &val,&b, a)==3)
+        if(strcmp(a,"CHILD")==0)
+            add_tree(root, val,CHILD, b);
+        else
+            if (strcpy(a, "BROTHERS")==0)
+                add_tree(root, val,BROTHERS, b);
+        print_tree(root, 0);
+        obhod(root, &nnode, &inner);
+        printf("%d node, %d leaves, %d inner\n", nnode, nnode-inner, inner);
+
+    if (wt){
+        fprintf(wt_to, "root %d\n", root->key);
+        categorization(wt_to, root);
+        fclose(wt_to);
+    }
+    return 0;
+}
+
+//создание корневого узла
+node* crnode(int val)
+{
+	node* root = (node*) malloc(sizeof(node));
+	root->key = val;
+	root->left = NULL;
+	root->right = NULL;
+	return root;
+}
+//функция добавления, которая будет получать на вход указатель на корень
+//дерева и ключ добавляемого элемента
+int add_tree(struct node *root, int val, int a, int b)
+{
+    struct node *dat;
+	if (a==CHILD)
+        dat=find_tree(root,b);
+    else
+    if (a==BROTHERS){
+        dat=roditeli(root,b);
+    }
+    if (((NULL!=dat->left)&&(NULL!=dat->right))||(NULL==dat))
+        return 1;
+    if (NULL==dat->left){
+        (dat->left)=crnode(val);
+    }
+    else if (NULL==dat->right){
+        (dat->right)=crnode(val);
+        }
+}
+
+//обход дерева
+int obhod (struct node *root, int *nnode, int *inner) {
+ 	if (NULL!=root) {
+ 		if ((NULL!=root->left) || (NULL!=root->right))
+ 			(*inner)++;
+ 		(*nnode)++;
+ 		obhod(root->left, nnode, inner);
+		obhod(root->right, nnode, inner);
+	}
+ }
+//функция поиска элемента, которая возвращает указатель на элемент, содержащий искомый ключ или NULL
+struct node* find_tree(struct node *root, int val)
+{
+    struct node*temp=0;
+	if (NULL == root)
+		return NULL;
+	if (val == root->key)
+		return root;
+	if ((temp=find_tree(root->left, val))!=0)
+        return temp;
+    if ((temp=find_tree(root->right, val))!=0)
+        return temp;
+    }
+//функция вывода массива по дереву
+void print_tree(struct node *root, int stage)
+{
+    int i;
+    for(i=0;i<stage;i++)
+        printf("    ");
+	if (NULL!=root) {
+		print_tree(root->left,stage+1);
+		printf("%d ", root->key);
+		print_tree(root->right,stage+1);
+	}
+}
+int categorization(FILE *file, struct node *root) {
+	if (NULL!=root) {
+ 		if (NULL!=root->left)
+			fprintf(file, "%d roditel %d\n", root->left->key, root->key);
+ 		if (NULL!=root->right)
+ 			fprintf(file, "%d roditel %d\n", root->right->key, root->key);
+		categorization(file, root->left);
+ 		categorization(file, root->right);
+	}
+}
+struct node * roditeli(struct node *root, int val) {
+    struct node*temp=0;
+        if (NULL==root)
+            return NULL;
+        else if (NULL!=root->left)
+        {
+                    if (root->left->key == val)
+ 				return temp=root;
+        }
+ 		else if (NULL!=root->right)
+        {
+                    if (root->right->key == val)
+ 				return temp=root;
+        }
+ 		roditeli(root->left, val);
+ 		roditeli(root->right, val);
+ }
+
+
